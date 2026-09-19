97070667f363c873a9c00a4c96e1f4b422d8db9e
kiran1423 <31442901+kiran1423@users.noreply.github.com>
2026-09-19 22:12:29 +0530

    Rename SSH key file and update kops command

--- kops.sh
+++ kops.sh
@@ -11,9 +11,9 @@ source .bashrc
 
 ssh-keygen
 
-cp /root/.ssh/id_rsa.pub my-keypair.pub
+cp /root/.ssh/id_rsa.pub my-key.pub
 
-chmod 777 my-keypair.pub
+chmod 777 my-key.pub
 
 vi kops.sh
 
@@ -25,7 +25,7 @@ mv kops-linux-amd64 /usr/local/bin/kops
 aws s3api create-bucket --bucket kiran-kops-testbkt.k8s.local --region us-east-1 --create-bucket-configuration LocationConstraint=us-east-1
 aws s3api put-bucket-versioning --bucket kiran-kops-testbkt.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://kiran-kops-testbkt.k8s.local
-kops create cluster --name=kiran.k8s.local --zones=us-east-1a --control-plane-count=1 --control-plane-size=t3.small --node-count=1 --node-size=t3.medium --node-volume-size=15 --control-plane-volume-size=15 --ssh-public-key=my-keypair.pub --image=ami-02d26659fd82cf299 --networking=calico --topology=public
+kops create cluster --name=kiran.k8s.local --zones=us-east-1a --control-plane-count=1 --control-plane-size=t3.small --node-count=1 --node-size=t3.medium --node-volume-size=15 --control-plane-volume-size=15 --ssh-public-key=my-key.pub --image=ami-02d26659fd82cf299 --networking=calico --topology=public
 kops update cluster --name kiran.k8s.local --yes --admin
 
 
